430971fbe9248672d8eaee5a2208c8917d84594f
KB Ruchitha <155042561+rcita13@users.noreply.github.com>
2024-11-07 12:39:09 +0530

    Delete main directory

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Topic, Recipe
-
-
-admin.site.register(Recipe)
-admin.site.register(Topic)
--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home,search,detail
-
-urlpatterns = [
-    path("",home, name="home"),
-    path("search", search, name="search"),
-    path("<slug>", detail, name="detail"),
-    ]
